73d505a5fac7a541e3f285b0e670e967373190b9
Adam de Vries <apbdevries@gmail.com>
2020-11-26 22:07:26 +0800

    done for today

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Bloody Mary")
+Cocktail.create(name: "Sex on the Beach")
+Cocktail.create(name: "Margarita")
+Cocktail.create(name: "Pina Colada")
+
+Ingredient.create(name: "Mint")
+Ingredient.create(name: "Vodka")
+Ingredient.create(name: "Rhum")
+Ingredient.create(name: "Tequila")
+Ingredient.create(name: "Orange Juice")
+Ingredient.create(name: "Tomato Juice")
+
+Dose.create(description: "6 leaves", cocktail_id: "1", ingredient_id: "1")
+Dose.create(description: "1.5 oz", cocktail_id: "1", ingredient_id: "3")
+Dose.create(description: "4.5 cl", cocktail_id: "2", ingredient_id: "2")
+Dose.create(description: "9 cl", cocktail_id: "2", ingredient_id: "6")
+Dose.create(description: "1.33 oz", cocktail_id: "3", ingredient_id: "2")
+Dose.create(description: "1.33 oz", cocktail_id: "3", ingredient_id: "5")
+Dose.create(description: "2 cl", cocktail_id: "4", ingredient_id: "4")
